4abd5d2116d871071831c1fe1776c9e01b872c61
SACHINSAHU9302 <sahusachinkumar60@gmail.com>
2026-02-25 11:49:25 +0530

    get profile update profile

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,4 +7,4 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.em
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import google_login
+from .views import user_login, get_profile, update_profile
 
 urlpatterns = [
     path("google-login/", google_login),
-    
+    path("login/", user_login),
+    path("profile/", get_profile),
+    path("profile/update/", update_profile),
 ]
